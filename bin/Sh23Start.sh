d899edca616b20630ca4c06f5e24608de71657b8
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2021-09-11 12:52:22 -0300

    Push

--- bin/Sh23Start.sh
+++ bin/Sh23Start.sh
@@ -23,12 +23,12 @@ apt install -y docker*
 # Bucket Download Files
 # https://objectstorage.sa-saopaulo-1.oraclecloud.com/p/g_n5KbHIcNffVmGHtswMY_5lLOhgIbJ-um-EURgrObFl1zCHem8dSBWRwYUPqVwp/n/grwodtg32n4d/b/Sirherobrine23MainBucket/o/
 
-if ! command -v code-insider &> /dev/null;
-  esac "$(uname -m)" in
+if ! command -v code-insider &> /dev/null; then
+  case "$(uname -m)" in
     amd64 ) wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -O /tmp/Code.deb;;
-    arm64 || aarch64 ) wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-arm64" -O /tmp/Code.deb;;
+    arm64 | aarch64 ) wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-arm64" -O /tmp/Code.deb;;
     i*86 ) wget "https://objectstorage.sa-saopaulo-1.oraclecloud.com/p/g_n5KbHIcNffVmGHtswMY_5lLOhgIbJ-um-EURgrObFl1zCHem8dSBWRwYUPqVwp/n/grwodtg32n4d/b/Sirherobrine23MainBucket/o/code-insiders_1.35.0-1559607154_i386.deb" -O /tmp/Code.deb;;
-  case
+  esac
   dpkg -i /tmp/Code.deb
   rm  -f /tmp/Code.deb
 fi
